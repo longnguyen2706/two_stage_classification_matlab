ceb4bef7844c82d428d344fbe581a8abefa62cc5
Long <ndlong95@gmail.com>
2018-08-22 14:09:50 +0800

    fix stage 2

--- Stage2_Classification.m
+++ Stage2_Classification.m
@@ -1,5 +1,5 @@
 function [stage2_predict_ts, stage2_predict_reject] = Stage2_Classification(total_data, total_label, tr_idx, ts_idx, reject_index)
-
+clear tr_data tr_label;
 %********Package********************
 addpath('liblinear-2.1\liblinear-2.1\matlab'); 
 
@@ -9,7 +9,7 @@ nclass = length(unique(total_label));
 
 %**********************************
 ts_data = total_data(ts_idx,:);
-ts_label = total_data(ts_idx, :);
+ts_label = total_label(ts_idx, :);
 tr_data = total_data(tr_idx,:);
 tr_label = total_label(tr_idx,:);
     
@@ -17,7 +17,7 @@ options = ['-C -s 2'];
 model2 = train(double(tr_label), sparse(tr_data), options);
 options = ['-c ',num2str(model2(1)),' -s 2'];
 model2 = train(double(tr_label), sparse(tr_data), options);
-[label] = predict(ts_label, sparse(ts_data), model2);
+[stage2_predict_ts] = predict(ts_label, sparse(ts_data), model2);
 
 reject_data = [];
 reject_label = [];
